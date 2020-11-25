dfb0e4df4ed6703d11caf3d384629ceaf322bde9
jiangyun <jiangjinjiu@i9i8.com>
2020-11-25 11:01:39 +0800

    设置熔断超时时间和ribbon超时时间

--- product-center/product-api/src/main/java/com/monkey/product/controller/rpc/ProductRpcController.java
+++ product-center/product-api/src/main/java/com/monkey/product/controller/rpc/ProductRpcController.java
@@ -1,9 +1,9 @@
 package com.monkey.product.controller.rpc;
 
 import com.monkey.common.response.Response;
+import com.monkey.product.service.impl.ProductElasticsearchService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductRpcController {
 
+    private final ProductElasticsearchService productElasticsearchService;
+
+    ProductRpcController(ProductElasticsearchService productElasticsearchService) {
+        this.productElasticsearchService = productElasticsearchService;
+    }
+
     /**
      * es 商品同步
      * @return Response
      */
     @RequestMapping(value = "/syncProductToEs", method = RequestMethod.POST)
     Response<String> syncProductToEs() {
+        this.productElasticsearchService.execute();
         return Response.ok("success");
     }
 }
